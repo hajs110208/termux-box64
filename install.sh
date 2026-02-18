32e491ead062f0822e3b73b3849e8bfb1a70a846
hajs0208 <156035391+hajs110208@users.noreply.github.com>
2026-02-18 12:50:14 +0900

    Add installation script for Box64 setup
    Install necessary packages and set up Box64 environment.

---
+++ install.sh
@@ -0,0 +1,22 @@
+echo installing packages
+pkg install x11-repo glibc-repo -y && pkg install pulseaudio wget glibc git xkeyboard-config freetype fontconfig libpng xorg-xrandr termux-x11-nightly termux-am zenity which bash curl sed cabextract -y --no-install-recommends
+clear
+sleep 3
+echo now grant storage permission
+termux-setup-storage
+sleep 3
+termux-setup-storage
+sleep 4
+clear
+echo downloading and unpacking glibc
+wget -q --show-progress https://github.com/Ilya114/Box64Droid/releases/download/alpha/glibc-prefix.tar.xz
+sleep 2
+clear
+tar -xJf glibc-prefix.tar.xz -C $PREFIX/
+echo setting up box64
+echo "export LD_PRELOAD=$PREFIX/glibc/lib/ld-linux-aarch64.so.1 && $PREFIX/glibc/bin/box64" > $PREFIX/bin/box64
+chmod +x $PREFIX/box64
+sleep 2
+echo now deleting trashes
+sleep 2
+rm glibc-prefix.tar.xz
