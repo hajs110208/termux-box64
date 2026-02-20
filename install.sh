17bb1c76bb24e725a9c5201e3488b11d5297661c
hajs0208 <156035391+hajs110208@users.noreply.github.com>
2026-02-20 21:57:54 +0900

    Modify install.sh to include box64-glibc support
    Updated the install script to install box64-glibc and modified the box64 command to accept multiple arguments.

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 echo installing packages
-pkg install x11-repo glibc-repo -y && pkg install pulseaudio wget glibc git xkeyboard-config freetype fontconfig libpng xorg-xrandr termux-x11-nightly termux-am zenity which bash curl sed cabextract -y --no-install-recommends
+pkg install x11-repo glibc-repo -y && pkg install glibc box64-glibc
 clear
 sleep 3
 echo now grant storage permission
@@ -13,7 +13,7 @@ pkg install glibc box64-glibc
 sleep 2
 clear
 echo setting up box64
-echo "export LD_PRELOAD=$PREFIX/glibc/lib/ld-linux-aarch64.so.1 && $PREFIX/glibc/bin/box64 $1" > $PREFIX/bin/box64
+echo "export LD_PRELOAD=$PREFIX/glibc/lib/ld-linux-aarch64.so.1 && $PREFIX/glibc/bin/box64 $1 $2 $3 $4 $5" > $PREFIX/bin/box64
 chmod +x $PREFIX/bin/box64
 sleep 2
 clear
